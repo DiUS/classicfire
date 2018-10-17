9f9681c0c602add2797d870733b52abd36428101
Sam Bergin <sbergin@dius.com.au>
2018-10-17 13:56:31 +1100

    Translate words

--- src/talky.js
+++ src/talky.js
@@ -1,10 +1,11 @@
 import AWS from 'aws-sdk'
 
-AWS.config.region = 'ap-southeast-2'
+AWS.config.region = 'us-east-1'
 AWS.config.accessKeyId = process.env.REACT_APP_ACCESS_KEY_ID
 AWS.config.secretAccessKey = process.env.REACT_APP_SECRET_ACCESS_KEY
 
 const polly = new AWS.Polly({apiVersion: '2016-06-10'})
+const translate = new AWS.Translate({apiVersion: '2017-07-01'})
 
 const getVoices = () => new Promise((resolve, reject) => {
   const params = {
@@ -16,13 +17,29 @@ const getVoices = () => new Promise((resolve, reject) => {
   })
 })
 
+const doTranslate = (words) => new Promise((resolve, reject) => {
+  const params = {
+    SourceLanguageCode: 'en',
+    TargetLanguageCode: 'es',
+    Text: words
+  }
+  return translate.translateText(params, (err, data) => {
+    if (err) {
+      console.error('err', err)
+    }
+    return resolve(data.TranslatedText)
+  })
+})
+
 export const talky = async (words) => {
+
+  const translatedWords = await doTranslate(words)
 
   const voiceIds = await getVoices()
 
   const params = {
     OutputFormat: 'mp3',
-    Text: words,
+    Text: translatedWords,
     VoiceId: voiceIds[0],
     SampleRate: '22050',
     TextType: 'text'
